120ff3c5a3f7689e4e8c53dd9afd5c33d4229cea
erikahernande <erika.hernandezmay@cbtis72.edu.mx>
2023-05-15 12:18:49 -0500

    proyecto

---
+++ qrreader/lib/pages/mapas_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/scan_list_provider.dart';
+import '../widgets/scan_tiles.dart';
+
+class MapasPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ScanTiles(
+      tipo: 'geo',
+    );
+  }
+}
